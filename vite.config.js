97507253568658d0d123359c999bf53b948c2a57
Marco Magni <155746143+rkoma93@users.noreply.github.com>
2024-11-22 20:57:37 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,12 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: 'src', // Set the root directory to 'src'
+  root: '/', // Root folder containing index.html
   build: {
-    outDir: '../dist', // Output directory for the built files
-    emptyOutDir: true
+    outDir: '../dist', // Output folder
+    emptyOutDir: true,
+    rollupOptions: {
+      input: 'index.html' // Specify the entry HTML file
+    }
   }
 });
