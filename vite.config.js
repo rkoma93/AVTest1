74d928d5abe979fa5d8ed33d109afbdd76c780c7
Marco Magni <155746143+rkoma93@users.noreply.github.com>
2024-11-22 18:03:05 +0100

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  root: './', // Make sure this points to the correct directory
-  build: {
-    outDir: 'dist', // Vercel will serve from 'dist' folder after build
-  },
-});
