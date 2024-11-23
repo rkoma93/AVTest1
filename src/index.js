25c881ce8d01b8ed6ad76a86f44af58dbb5c4a55
Marco Magni <155746143+rkoma93@users.noreply.github.com>
2024-11-23 09:38:46 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,9 +5,3 @@ const map = initializeMap();
 
 // Initialize the tracker to display real-time vehicle positions
 initializeTracker(map);
-
-import RBush from 'rbush';
-
-const tree = new RBush();
-tree.insert({ minX: 0, minY: 0, maxX: 1, maxY: 1 });
-console.log(tree.all());
